9fc8acedc5ad23a2a56e44cc169f65088aaa7755
Ning Sun <sunng@about.me>
2016-10-01 08:39:58 +0800

    Version 0.3.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject diehard "0.3.2"
+(defproject diehard "0.3.3-SNAPSHOT"
   :description "A Failsafe wrapper for Clojure"
   :url "http://github.com/sunng87/diehard"
   :license {:name "Eclipse Public License"

1b3235f7a43fd5f846ac717929db67e32c3fd3ff
Ning Sun <sunng@about.me>
2016-11-04 19:13:15 +0800

    (chore) version bump
    Signed-off-by: Ning Sun <sunng@about.me>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject diehard "0.3.6-SNAPSHOT"
+(defproject diehard "0.4.0-SNAPSHOT"
   :description "A Failsafe wrapper for Clojure"
   :url "http://github.com/sunng87/diehard"
   :license {:name "Eclipse Public License"

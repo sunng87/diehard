ceced62dd18eb2f448aaef24272fad908358fb19
Ning Sun <sunng@about.me>
2016-11-22 23:47:20 +0800

    (feat) update to failsafe 1.0.0
    Signed-off-by: Ning Sun <sunng@about.me>

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject diehard "0.4.1-SNAPSHOT"
+(defproject diehard "0.5.0-SNAPSHOT"
   :description "A Failsafe wrapper for Clojure"
   :url "http://github.com/sunng87/diehard"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [net.jodah/failsafe "0.9.5"]]
+                 [net.jodah/failsafe "1.0.0"]]
   :plugins [[lein-codox "0.9.5"]]
   :codox {:output-path "target/codox"
           :source-uri "https://github.com/sunng87/diehard/blob/master/{filepath}#L{line}"

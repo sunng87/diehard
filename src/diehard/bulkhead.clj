0e9fcf54899811e0016ab311a6034be8d52b0152
Ning Sun <sunng@about.me>
2017-09-30 19:32:09 +0800

    (test) added tests for bulkhead
    Signed-off-by: Ning Sun <sunng@about.me>

--- src/diehard/bulkhead.clj
+++ src/diehard/bulkhead.clj
@@ -9,19 +9,22 @@
   IBulkhead
   (acquire! [this] (.acquire ^Semaphore semaphore))
   (acquire! [this timeout-ms]
-    (when-not (.tryAcquire semaphore timeout-ms TimeUnit/MILLISECONDS)
+    (when-not (.tryAcquire ^Semaphore semaphore timeout-ms TimeUnit/MILLISECONDS)
       (throw (ex-info "Failed to acquire bulkhead" {::bulkhead :failed
                                                     ::timeout timeout-ms}))))
   (release! [this]
     (.release ^Semaphore semaphore)))
 
 (defn bulkhead [concurrency]
-  (Bulkhead. concurrency))
+  (Bulkhead. (Semaphore. concurrency)))
 
 (defmacro with-bulkhead [bulkhead-opts & body]
-  `(let [{bulkhead :bulkhead} bulkhead-opts]
-     (acquire! bulkhead)
+  `(let [{bulkhead# :bulkhead
+          timeout-ms# :timeout-ms} ~bulkhead-opts]
+     (if (nil? timeout-ms#)
+       (acquire! bulkhead#)
+       (acquire! bulkhead# timeout-ms#))
      (try
        ~@body
        (finally
-         (release! bulkhead)))))
+         (release! bulkhead#)))))

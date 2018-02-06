517f65f05b58afe759ae3850c0f379b742079171
Ning Sun <sunng@protonmail.com>
2018-02-06 23:12:31 +0800

    (doc) generate docs
    Signed-off-by: Ning Sun <sunng@protonmail.com>

--- project.clj
+++ project.clj
@@ -6,9 +6,9 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [net.jodah/failsafe "1.0.5"]
                  [org.clojure/spec.alpha "0.1.143"]]
-  :plugins [[lein-codox "0.9.5"]
+  :plugins [[lein-codox "0.10.3"]
             [lein-eftest "0.4.0"]]
-  :codox {:output-path "target/codox"
+  :codox {:output-path "docs/"
           :source-uri "https://github.com/sunng87/diehard/blob/master/{filepath}#L{line}"
           :metadata {:doc/format :markdown}}
   :deploy-repositories {"releases" :clojars})

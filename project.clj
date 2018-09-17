2ecc1fe4fd6272621a1243378a3f19bb54940ada
Ning Sun <sunng@protonmail.com>
2018-09-17 21:33:47 +0800

    (chore) update deps

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [net.jodah/failsafe "1.0.5"]
-                 [org.clojure/spec.alpha "0.1.143"]]
+                 [net.jodah/failsafe "1.1.0"]
+                 [org.clojure/spec.alpha "0.2.176"]]
   :plugins [[lein-codox "0.10.3"]
             [lein-eftest "0.5.2"]]
   :codox {:output-path "docs/"

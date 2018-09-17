62225d62543e8bdea7febcc4a344169ac179f458
Ning Sun <sunng@protonmail.com>
2018-09-17 21:20:09 +0800

    (chore) update eftest

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [net.jodah/failsafe "1.0.5"]
                  [org.clojure/spec.alpha "0.1.143"]]
   :plugins [[lein-codox "0.10.3"]
-            [lein-eftest "0.4.0"]]
+            [lein-eftest "0.5.2"]]
   :codox {:output-path "docs/"
           :source-uri "https://github.com/sunng87/diehard/blob/master/{filepath}#L{line}"
           :metadata {:doc/format :markdown}}

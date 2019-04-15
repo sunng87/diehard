21e6f955f64fbae5bce459fbf0349627ace3088f
Ning Sun <sunng@protonmail.com>
2019-04-16 00:14:26 +0800

    Version 0.8.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject diehard "0.8.3"
+(defproject diehard "0.8.4-SNAPSHOT"
   :description "Safety utilities for Clojure"
   :url "http://github.com/sunng87/diehard"
   :license {:name "Eclipse Public License"

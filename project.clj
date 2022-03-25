73759a1cfcd9136e26f04ed53742b93d84f9e1a9
Ning Sun <sunng@protonmail.com>
2022-03-25 23:00:44 +0800

    feat: clojure 1.11

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/sunng87/diehard"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.0"]
                  [dev.failsafe/failsafe "3.2.3"]
                  [org.clojure/spec.alpha "0.3.218"]]
   :plugins [[lein-codox "0.10.7"]

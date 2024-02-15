9be2e644836322074fe5cd72d178cd99584203da
Gene Kim <genek@itrevolution.com>
2024-02-15 12:14:28 -0800

    test added to test rate-limits of less than 1.0

--- test/diehard/rate_limiter_test.clj
+++ test/diehard/rate_limiter_test.clj
@@ -20,4 +20,23 @@
       (.shutdown pool)
       (t/is (< (Math/abs (- @counter (* rate time-secs)))
                ;; error tolerance 3%
-               (* 0.03 (* rate time-secs)))))))
+               (* 0.03 (* rate time-secs))))))
+
+  (t/testing "rate limits less than 1.0"
+    (let [rate 0.5
+          threads 1
+          pool (Executors/newFixedThreadPool threads)
+          rl (r/rate-limiter {:rate rate})
+          counter (atom 0)
+          time-secs 5]
+      (doseq [_ (range threads)]
+        (.submit pool (cast Runnable (fn []
+                                       (while true
+                                         (r/acquire! rl)
+                                         (swap! counter inc))))))
+      (Thread/sleep (* time-secs 1000))
+      (.shutdown pool)
+      (let [variance (Math/abs (- @counter (* rate time-secs)))]
+        (t/is (<= variance
+                ;; error tolerance: 0.5  -- larger, because we are dealing with slower ticks
+                0.5))))))

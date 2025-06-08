fff7921da430c3934b82975f789e91faee66d3c9
Mark Sto <marksto@yourown.software>
2025-06-08 17:34:09 +0400

    Add test cases for uninterruptible sleep

--- test/diehard/rate_limiter_test.clj
+++ test/diehard/rate_limiter_test.clj
@@ -1,42 +1,98 @@
 (ns diehard.rate-limiter-test
-  (:require  [clojure.test :as t]
-             [diehard.rate-limiter :as r])
-  (:import [java.util.concurrent Executors]))
+  (:require [clojure.test :refer [deftest testing is]]
+            [diehard.rate-limiter :as rl])
+  (:import [java.time Duration]
+           [java.util.concurrent ExecutorService Executors Future]))
 
-(t/deftest rate-limiter-test
-  (t/testing "base case"
-    (let [rate 1000
-          threads 32
-          pool (Executors/newFixedThreadPool threads)
-          rl (r/rate-limiter {:rate rate})
-          counter (atom 0)
-          time-secs 2]
-      (doseq [_ (range threads)]
-        (.submit pool (cast Runnable (fn []
-                                       (while true
-                                         (r/acquire! rl)
-                                         (swap! counter inc))))))
-      (Thread/sleep (* time-secs 1000))
-      (.shutdown pool)
-      (t/is (< (Math/abs (- @counter (* rate time-secs)))
-               ;; error tolerance 3%
-               (* 0.03 (* rate time-secs))))))
+(def default-error
+  "Relative error in percent. 3% by default."
+  0.03)
 
-  (t/testing "rate limits less than 1.0"
-    (let [rate 0.5
-          threads 1
-          pool (Executors/newFixedThreadPool threads)
-          rl (r/rate-limiter {:rate rate})
-          counter (atom 0)
-          time-secs 5]
-      (doseq [_ (range threads)]
-        (.submit pool (cast Runnable (fn []
-                                       (while true
-                                         (r/acquire! rl)
-                                         (swap! counter inc))))))
-      (Thread/sleep (* time-secs 1000))
-      (.shutdown pool)
-      (let [variance (Math/abs (- @counter (* rate time-secs)))]
-        (t/is (<= variance
-                ;; error tolerance: 0.5  -- larger, because we are dealing with slower ticks
-                  0.5))))))
+(defn- approx==
+  ([expected actual]
+   (approx== expected actual (* default-error expected)))
+  ([expected actual tolerance]
+   (< (abs (- expected actual)) tolerance)))
+
+(defn- submit
+  ^Future [^ExecutorService executor ^Callable f]
+  (ExecutorService/.submit executor f))
+
+(deftest rate-limiter-test
+  (testing "base case"
+    (testing "interruptible sleep"
+      (let [rate 1000
+            rate-limiter (rl/rate-limiter {:rate rate})
+
+            threads 32
+            pool (Executors/newFixedThreadPool threads)
+
+            counter (atom 0)
+            time-secs 2]
+        (doseq [_ (range threads)]
+          (submit pool (fn []
+                         (while true
+                           (rl/acquire! rate-limiter)
+                           (swap! counter inc)))))
+        (Thread/sleep (Duration/ofSeconds time-secs))
+        (ExecutorService/.shutdown pool)
+        (is (approx== (* rate time-secs) @counter))))
+
+    (testing "uninterruptible sleep"
+      (let [rate 1000
+            rate-limiter (rl/rate-limiter {:rate     rate
+                                           :sleep-fn rl/uninterruptible-sleep})
+
+            threads 32
+            pool (Executors/newFixedThreadPool threads)
+
+            counter (atom 0)
+            time-secs 2]
+        (doseq [_ (range threads)]
+          (submit pool (fn []
+                         (while true
+                           (rl/acquire! rate-limiter)
+                           (swap! counter inc)))))
+        (Thread/sleep (Duration/ofSeconds time-secs))
+        (ExecutorService/.shutdown pool)
+        (is (approx== (* rate time-secs) @counter)))))
+
+  (testing "rate less than 1.0"
+    (testing "interruptible sleep"
+      (let [rate 0.5
+            rate-limiter (rl/rate-limiter {:rate rate})
+
+            threads 32
+            pool (Executors/newFixedThreadPool threads)
+
+            counter (atom 0)
+            time-secs 5]
+        (doseq [_ (range threads)]
+          (submit pool (fn []
+                         (while true
+                           (rl/acquire! rate-limiter)
+                           (swap! counter inc)))))
+        (Thread/sleep (Duration/ofSeconds time-secs))
+        (ExecutorService/.shutdown pool)
+        ;; exact error tolerance, since we are dealing with much slower ticks
+        (is (approx== (* rate time-secs) @counter 1))))
+
+    (testing "uninterruptible sleep"
+      (let [rate 0.5
+            rate-limiter (rl/rate-limiter {:rate     rate
+                                           :sleep-fn rl/uninterruptible-sleep})
+
+            threads 32
+            pool (Executors/newFixedThreadPool threads)
+
+            counter (atom 0)
+            time-secs 5]
+        (doseq [_ (range threads)]
+          (submit pool (fn []
+                         (while true
+                           (rl/acquire! rate-limiter)
+                           (swap! counter inc)))))
+        (Thread/sleep (Duration/ofSeconds time-secs))
+        (ExecutorService/.shutdown pool)
+        ;; exact error tolerance, since we are dealing with much slower ticks
+        (is (approx== (* rate time-secs) @counter 1))))))

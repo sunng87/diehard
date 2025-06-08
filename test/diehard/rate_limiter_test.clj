bc69c83e9e70b87d42e74780d76632e9d99ad2fd
Mark Sto <marksto@yourown.software>
2025-06-08 18:02:11 +0400

    Showcase that scheduled tasks do not get terminated
    and thus start affecting each other due to threads depletion (not good wrt isolation)

--- test/diehard/rate_limiter_test.clj
+++ test/diehard/rate_limiter_test.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer [deftest testing is]]
             [diehard.rate-limiter :as rl])
   (:import [java.time Duration]
-           [java.util.concurrent ExecutorService Executors Future]))
+           [java.util.concurrent ExecutorService Executors Future TimeUnit]))
 
 (def default-error
   "Relative error in percent. 3% by default."
@@ -31,7 +31,9 @@
                        (swap! counter inc)))))
     (Thread/sleep (Duration/ofSeconds run-time-sec))
     (ExecutorService/.shutdown pool)
-    @counter))
+    (let [terminated? (ExecutorService/.awaitTermination pool 1 TimeUnit/SECONDS)]
+      {:terminated? terminated?
+       :count       @counter})))
 
 (deftest rate-limiter-test
   (testing "high rates"
@@ -39,31 +41,47 @@
       (let [rate 1000
             rate-limiter (rl/rate-limiter {:rate rate})
             time-sec 2
-            count (run-rate-limited-counting rate-limiter time-sec)]
-        (is (approx== (* rate time-sec) count))))
+            {:keys [terminated? count]} (run-rate-limited-counting rate-limiter
+                                                                   time-sec)]
+        (is terminated?
+            "Terminates successfully, without timeout")
+        (is (approx== (* rate time-sec) count)
+            "Count must be close to an expected value")))
 
     (testing "uninterruptible sleep"
       (let [rate 1000
             rate-limiter (rl/rate-limiter {:rate     rate
                                            :sleep-fn rl/uninterruptible-sleep})
             time-sec 2
-            count (run-rate-limited-counting rate-limiter time-sec)]
-        (is (approx== (* rate time-sec) count)))))
+            {:keys [terminated? count]} (run-rate-limited-counting rate-limiter
+                                                                   time-sec)]
+        (is terminated?
+            "Terminates successfully, without timeout")
+        (is (approx== (* rate time-sec) count)
+            "Count must be close to an expected value"))))
 
   (testing "low rates (less than 1.0)"
     (testing "interruptible sleep"
       (let [rate 0.5
             rate-limiter (rl/rate-limiter {:rate rate})
             time-sec 5
-            count (run-rate-limited-counting rate-limiter time-sec)]
+            {:keys [terminated? count]} (run-rate-limited-counting rate-limiter
+                                                                   time-sec)]
+        (is terminated?
+            "Terminates successfully, without timeout")
         ;; exact error tolerance, since we are dealing with much slower ticks
-        (is (approx== (* rate time-sec) count 1))))
+        (is (approx== (* rate time-sec) count 1)
+            "Count must be close to an expected value")))
 
     (testing "uninterruptible sleep"
       (let [rate 0.5
             rate-limiter (rl/rate-limiter {:rate     rate
                                            :sleep-fn rl/uninterruptible-sleep})
             time-sec 5
-            count (run-rate-limited-counting rate-limiter time-sec)]
+            {:keys [terminated? count]} (run-rate-limited-counting rate-limiter
+                                                                   time-sec)]
+        (is terminated?
+            "Terminates successfully, without timeout")
         ;; exact error tolerance, since we are dealing with much slower ticks
-        (is (approx== (* rate time-sec) count 1))))))
+        (is (approx== (* rate time-sec) count 1)
+            "Count must be close to an expected value")))))
